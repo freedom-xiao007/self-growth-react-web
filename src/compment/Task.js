c93576e094c6f9fc95a29bfa460b08b20fa04e74
萧 <1243925457@qq.com>
2021-10-29 07:24:42 +0800

    初步能发送请求

--- src/compment/Task.js
+++ src/compment/Task.js
@@ -2,10 +2,12 @@ import { List} from 'antd';
 import React from "react";
 import TaskLabel from "./TaskLable";
 import TaskDetail from "./TaskDetail";
+import axios from 'axios';
 
 export class Task extends React.Component {
-    render() {
-        const data = [
+    constructor(props) {
+        super(props);
+        this.data = [
             {
                 "group": "taskGroup",
                 "taskList": [
@@ -13,12 +15,21 @@ export class Task extends React.Component {
                 ]
             }
         ];
+    }
 
+    componentDidMount() {
+        const apiUrl = 'http://localhost:8080/v1/task/listByGroup';
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => console.log('This is your data', data));
+    }
+
+    render() {
         return (
             <List
                 size="large"
                 bordered
-                dataSource={data}
+                dataSource={this.data}
                 renderItem={item => <List.Item>
                     <List
                         size="large"
