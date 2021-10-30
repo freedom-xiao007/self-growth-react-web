95579d05b9dc1307701ad7795ae3582fe8843945
萧 <1243925457@qq.com>
2021-10-30 22:15:34 +0800

    使用统一的ip地址

--- src/api/taskRequest.js
+++ src/api/taskRequest.js
@@ -2,14 +2,14 @@ import request from "./request";
 
 export function getTaskListByGroup() {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/listByGroup",
+        url: "/v1/task/listByGroup",
         method: "get"
     });
 }
 
 export function addTaskGroup(groupName) {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/addTaskGroup",
+        url: "/v1/task/addTaskGroup",
         method: "post",
         data: groupName,
     })
@@ -17,7 +17,7 @@ export function addTaskGroup(groupName) {
 
 export function addNewTask(newTask) {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/add",
+        url: "/v1/task/add",
         method: "post",
         data: newTask,
     })
@@ -25,28 +25,28 @@ export function addNewTask(newTask) {
 
 export function completeTaskById(id) {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/complete/" + id,
+        url: "/v1/task/complete/" + id,
         method: "post",
     })
 }
 
 export function deleteTaskGroupByName(name) {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/deleteGroup/" + name,
+        url: "/v1/task/deleteGroup/" + name,
         method: "post",
     })
 }
 
 export function deleteTaskById(id) {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/deleteTask/" + id,
+        url: "/v1/task/deleteTask/" + id,
         method: "post",
     })
 }
 
 export function modifyGroupById(group) {
     return request({
-        url: "http://127.0.0.1:8080/v1/task/modifyGroup",
+        url: "/v1/task/modifyGroup",
         method: "post",
         data: group,
     })
